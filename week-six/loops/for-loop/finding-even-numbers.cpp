5fcb5c8f5e63c1701615451008a70646e1f44f43
tremainebuchanan <tremainekbuchanan@gmail.com>
2022-04-13 19:24:51 -0500

    for loops

---
+++ week-six/loops/for-loop/finding-even-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/* Write a program that will find all even numbers and odd numbers between 1 and 10 (inclusive). */
+int main(){
+   
+    for(int i = 1; i < 11;i++){
+        if(i % 2 == 0){
+            cout << i << " is even." << endl;
+        }else{
+            cout << i << " is odd."  << endl;
+        }
+    }
+    
+    return 0;
+}
---
+++ week-six/loops/for-loop/for-loop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // For loop
+    // Three components of the for loop 
+    // 1 - initialization (optional) - where are you starting your loop. 
+    // 2 - condition (mandatory) 
+    //      - the loop will become infinite without a condition.
+    //      - the condition is required such that the loop will 
+    //        execute. 
+    // 3 - incrementation or update (mandatory)
+    //     - prevents the loop from being infinite
+    //     - changes the value of the loop variable such that the condition 
+    //     can ultimately be met to stop. 
+    
+    for(int i = 1; i < 11;i++){
+        cout << i << endl;
+    }
+    
+    return 0;
+}
